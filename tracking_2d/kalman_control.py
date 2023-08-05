d711671fbfefc79d99a60c6f8549072f599f3bde
Attila Zsiros <attila.zsiros@mail.muni.cz>
2023-08-05 12:23:43 +0200

    refactoring

--- tracking_2d/kalman_control.py
+++ tracking_2d/kalman_control.py
@@ -7,38 +7,38 @@ class KalmanFilter():
         self.acc_x = acc_x
         self.acc_y = acc_y
 
-        self.x = np.matrix([
+        self.x = np.array([
             [0],  # x
             [0],  # x'
             [0],  # y
             [0],  # y'
         ])
 
-        self.u = np.matrix([
+        self.u = np.array([
             [acc_x],
             [acc_y]
         ])
 
-        self.A = np.matrix([
+        self.A = np.array([
             [1, dt, 0, 0],
             [0, 1, 0, 0],
             [0, 0, 1, dt],
             [0, 0, 0, 1],
         ])
 
-        self.B = np.matrix([
+        self.B = np.array([
             [dt**2 / 2, 0],
             [dt, 0],
             [0, dt**2 / 2],
             [0, dt]
         ])
 
-        self.H = np.matrix([
+        self.H = np.array([
             [1, 0, 0, 0],
             [0, 0, 1, 0]
         ])
 
-        self.Q = np.matrix([
+        self.Q = np.array([
             [dt**4 / 4, dt**3 / 2, 0, 0],
             [dt**3 / 2, dt**2, 0, 0],
             [0, 0, dt**4 / 4, dt**3 / 2],
@@ -51,7 +51,7 @@ class KalmanFilter():
 
     @property
     def u_dec(self):
-        return np.matrix([
+        return np.array([
             [-self.x[1].item()],
             [-self.x[3].item()]
         ])
@@ -59,40 +59,39 @@ class KalmanFilter():
     def predict(self, decelerate=False):
         if decelerate:
             self.u = self.u_dec
-        self.x = np.dot(self.A, self.x) + np.dot(self.B, self.u)
+        self.x = self.A @ self.x + self.B @ self.u
 
         # P = A * P * A' + Q
-        self.P = np.dot(np.dot(self.A, self.P), self.A.T) + self.Q
+        self.P = self.A @ self.P @ self.A.T + self.Q
 
         return self.x[0].item(), self.x[2].item()
 
     def update(self, x_meas, y_meas):
         # K = P * H' * inv(H * P * H' + R)
-        HPH = np.dot(self.H, np.dot(self.P, self.H.T))
-        K = np.dot(self.P, np.dot(self.H.T, np.linalg.inv(HPH + self.R)))
+        S = np.linalg.inv(self.H @ self.P @ self.H.T + self.R)
+        K = self.P @ self.H.T @ S
 
-        z = np.matrix([
+        z = np.array([
             [x_meas],
             [y_meas]
         ])
-        self.x = self.x + np.dot(K, z - np.dot(self.H, self.x))
+        self.x = self.x + K @ (z - self.H @ self.x)
 
-        I = np.eye(self.H.shape[1])
-        I_KH = I - np.dot(K, self.H)
-        KRK = np.dot(K, np.dot(self.R, K.T))
         # P = (I - K * H) * P * (I - K * H)' + K * R * K
-        self.P = np.dot(I_KH, np.dot(self.P, I_KH.T)) + KRK
+        I = np.eye(self.H.shape[1])
+        I_KH = I - (K @ self.H)
+        self.P = I_KH @ self.P @ I_KH.T + K @ self.R @ K.T
         return K
 
     def toggle_acceleration(self):
-        sgn = np.matrix([
+        sgn = np.array([
             [-np.sign(np.round(self.x[1].item()))],
             [-np.sign(np.round(self.x[3].item()))]
         ])
         self.u = np.multiply(self.u, sgn)
 
     def reset_acceleration(self):
-        self.u = np.matrix([
+        self.u = np.array([
             [self.acc_x],
             [self.acc_y]
         ])
